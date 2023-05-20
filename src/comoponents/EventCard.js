c3318fbcc71ba22ad361aa825f4086ba68b907c5
Kevin Cain <kevin_cain@hotmail.co.uk>
2023-05-20 13:13:45 +0100

    search results almost done. Just need to format date and sort grid layout out

--- src/comoponents/EventCard.js
+++ src/comoponents/EventCard.js
@@ -1,5 +1,32 @@
-const EventCard = () => {
-    return <div>event card to live here</div>
+import { NavLink } from "react-router-dom";
+import { FaCalendar, FaMapPin  } from 'react-icons/fa'
+import { format } from "date-fns";
+
+const EventCard = (details) => {
+
+    console.log(details)
+    return (
+            <div className="max-w-sm rounded-sm shadow-lg border border-neutral-200 text-gray-500">
+                <img className="h-56 w-full" src={details.details.imageurl} alt="Event" />
+                <div class="px-6 py-4">
+                    <h3 class="font-bold text-base mb-2 text-center">{details.details.eventname}</h3>
+                    <p class="text-sm mb-6">
+                        {details.details.description}
+                    </p>
+                    <p className='mb-2 flex items-center text-sm'>
+                        <FaMapPin className="mr-2"/>
+                        {details.details.venue.name}, {details.details.venue.town}
+                    </p>
+                    <p className='mb-6 flex items-center text-sm'>
+                        <FaCalendar className="mr-2"/>
+                        {details.details.startdate} {/* need to format this */}
+                    </p>
+                    <NavLink to={`/event/${details.details.id}`}>
+                        <button className="rounded-md h-[50px] w-full border-none bg-teal-400 text-white">View details</button>
+                    </NavLink>
+                </div>
+            </div>
+    )
 }
 
 export default EventCard
--- src/pages/events/SearchPage/index.js
+++ src/pages/events/SearchPage/index.js
@@ -18,29 +18,31 @@ const SearchPage = () => {
 
     return ( 
         <>
-        <div className="w-full bg-teal-400 flex justify-center flex-col items-center">
-            <img className="h-[180px] w-[400px]"
-                src="https://d1plawd8huk6hh.cloudfront.net/assets/logo/png/skiddle-logo-white-landscape.png"
-                alt="skiddle">
-            </img>
-            <input
-                className='h-[50px] w-[400px] mb-4 p-2 rounded-md'
-                type="text"
-                placeholder="Search for events"
-                value={searchInput}
-                onChange={handleChange}
-            />
-        </div>
-        <div className='w-full'>
-            {searchResults && searchResults.length > 0 ?
-                searchResults.map(event => (
-                    <EventCard details={event} />
-                ))
-            : 
-                <div className='text-lg p-4 flex justify-center'>No results available, please try another search.</div>
-            }
-        </div>
-     </>
+            <div className="w-full bg-teal-400 flex justify-center flex-col items-center">
+                <img className="h-[180px] w-[400px]"
+                    src="https://d1plawd8huk6hh.cloudfront.net/assets/logo/png/skiddle-logo-white-landscape.png"
+                    alt="skiddle">
+                </img>
+                <input
+                    className='h-[50px] w-[400px] mb-4 p-2 rounded-md'
+                    type="text"
+                    placeholder="Search for events"
+                    value={searchInput}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className='my-8'>
+                {searchResults && searchResults.length > 0 ?
+                    searchResults.map(event => (
+                        <div class="container m-auto grid grid-cols-3 gap-4">
+                          <EventCard details={event} key={event.id}/>
+                        </div>
+                    ))
+                : 
+                    <div className='text-lg p-4 flex justify-center'>No results available, please try another search.</div>
+                }
+            </div>
+        </>
     )
 }
 
